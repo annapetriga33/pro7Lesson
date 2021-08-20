6bd33be51e99e52a7a76cee28415428ff723ff8c
annapetriga33 <83228999+annapetriga33@users.noreply.github.com>
2021-08-20 18:30:58 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,5 +1,7 @@
-var i = 1
-
-for(i = 1; i <= 10; i++) {
-    console.log(i + ' * 1 = ' + i);
-}
+for (var n = 1; n <= 9; n++){  
+    console.log("--/--/--/--/--/"); 
+    for (var m = 1; m <=9; m++)  
+    { 
+    console.log(n + " * " + m + " = " +(n*m)); 
+    } 
+} 
